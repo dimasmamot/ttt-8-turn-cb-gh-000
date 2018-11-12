482d2336ab54350974c4fc733e1748be5b627252
Dimas Rizky <dimas.rizky.hp@gmail.com>
2018-11-12 08:19:22 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,23 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8] }"
 end
+
+def valid_move?(board, index)
+  if index.between?(0,8)
+    if position_taken?(board, index)
+      return false
+    else
+      return true
+    end
+  else
+    return false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == "" | board [index] == " " | board[index] == nil
+    return false
+  else
+    return true
+  end
+end
